e43610f4b5ec74c2c8316d4c9b7a1fb20966b343
khanhbui-fullstackdev <bvkhanh0502@gmail.com>
2026-07-11 15:26:45 +0700

    Completed on solving 88

--- neetcode/array/array.go
+++ neetcode/array/array.go
@@ -83,12 +83,13 @@ func removeElementV1(nums []int, val int) int {
 }
 
 func RunMerge() {
-	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums1 := []int{1, 2, 8, 0, 0, 0}
 	m := 3
 	nums2 := []int{2, 5, 6}
 	n := 3
 	mergeUsingCopy(nums1, m, nums2, n)
 	mergeUsingExtraMemory(nums1, m, nums2, n)
+	merge(nums1, m, nums2, n)
 
 	nums1 = []int{1}
 	m = 1
@@ -96,12 +97,15 @@ func RunMerge() {
 	n = 0
 	mergeUsingCopy(nums1, m, nums2, n)
 	mergeUsingExtraMemory(nums1, m, nums2, n)
+	merge(nums1, m, nums2, n)
+
 	nums1 = []int{0}
 	m = 0
 	nums2 = []int{1}
 	n = 1
 	mergeUsingCopy(nums1, m, nums2, n)
 	mergeUsingExtraMemory(nums1, m, nums2, n)
+	merge(nums1, m, nums2, n)
 }
 
 /*
@@ -118,7 +122,7 @@ func mergeUsingCopy(nums1 []int, m int, nums2 []int, n int) {
 	// target: nums1
 	copy(nums1[m:], nums2)
 
-	fmt.Printf("\n After executing -> Nums1:%v", nums1)
+	fmt.Printf("After executing -> Nums1:%v", nums1)
 
 	// Sorting
 	slices.SortFunc(nums1, func(a, b int) int {
@@ -128,36 +132,71 @@ func mergeUsingCopy(nums1 []int, m int, nums2 []int, n int) {
 	fmt.Println()
 }
 
+/*
+Create new extra array called mergedNums
+then we will compare value between nums1[idx] && nums2[jdx].
+We simply check nums1[idx]<=num2s[jdx] // Brute Force technique -> add nums1[idx] into arr mergedNums
+Otherwise, we -> add num2s[jdx]  into arr mergedNums
+
+Brute Force technique
+Add the remaining items from nums1 into extra array. Similarly to num2s
+*/
 func mergeUsingExtraMemory(nums1 []int, m int, nums2 []int, n int) {
 	fmt.Println("*** Merge using extra memory ***")
-	mergedNums := make([]int, 0, len(nums1))
-
 	idx := 0
 	jdx := 0
-
+	mergedSortedNums := make([]int, 0, len(nums1))
 	for idx < m && jdx < n {
 		num1 := nums1[idx]
 		num2 := nums2[jdx]
+
 		if num1 <= num2 {
-			mergedNums = append(mergedNums, num1)
+			mergedSortedNums = append(mergedSortedNums, num1)
 			idx++
 		} else {
-			mergedNums = append(mergedNums, num2)
+			mergedSortedNums = append(mergedSortedNums, num2)
 			jdx++
 		}
 	}
 
 	for idx < m {
-		mergedNums = append(mergedNums, nums1[idx])
+		mergedSortedNums = append(mergedSortedNums, nums1[idx])
 		idx++
 	}
 
 	for jdx < n {
-		mergedNums = append(mergedNums, nums2[jdx])
+		mergedSortedNums = append(mergedSortedNums, nums2[jdx])
 		jdx++
 	}
 
-	nums1 = mergedNums
+	copy(nums1, mergedSortedNums)
+	fmt.Println("Merged nums:", nums1)
+	fmt.Println()
+}
+
+/*
+Using 3 pointers
+*/
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	fmt.Println("*** Merge using 3 pointers ***")
+	updatedIndex := len(nums1) - 1
+
+	// start to loop from the end
+	idx := m - 1
+	jdx := n - 1
+
+	// becasue we want to copy all of value/element from nums2 to nums1, so that we need to loop while j>=0
+	for jdx >= 0 {
+		if idx >= 0 && nums1[idx] >= nums2[jdx] {
+			// pick the higher value num1
+			nums1[updatedIndex] = nums1[idx]
+			idx--
+		} else {
+			nums1[updatedIndex] = nums2[jdx]
+			jdx--
+		}
+		updatedIndex--
+	}
 	fmt.Println("Merged nums:", nums1)
 	fmt.Println()
 }
